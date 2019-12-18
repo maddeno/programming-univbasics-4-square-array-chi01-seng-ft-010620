58de82f9099ce4284da3e0efbf538a8cf3040819
Olivia Madden <madden.livi@gmail.com>
2019-12-18 20:14:15 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,8 @@ def square_array(array)
   counter = 0
   new_array = []
   while counter<array.length do
-   new_array+= puts array[counter]**2
+   puts array[counter]**2
+   
     counter += 1 
   end
   new_array
